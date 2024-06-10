cbbbb52e9697e90a7d61b243550bf276660e34b8
Adronn_ <adilbaygabulov@gmail.com>
2024-06-10 18:01:51 +0500

    test1

---
+++ pages/basemap2.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import geemap.foliumap as geemap
+
+st.title("Bekzhan's Interactive Map")
+
+col1, col2 = st.columns([1, 1])
+options = list(geemap.basemaps.keys())
+index = options.index("OpenTopoMap")
+
+with col2:
+
+    basemap = st.selectbox("Select a basemap:", options, index)
+
+
+with col1:
+
+    m = geemap.Map()
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
