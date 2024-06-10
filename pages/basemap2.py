b247539210f243d3c565e72d7e2bf6e70c7ac260
Adronn_ <adilbaygabulov@gmail.com>
2024-06-10 18:09:37 +0500

    test1

--- pages/basemap2.py
+++ pages/basemap2.py
@@ -7,8 +7,7 @@ st.selectbox('test', a)
 col1, col2 = st.columns([4, 1])
 options = list(geemap.basemaps.keys())
 index = options.index("OpenTopoMap")
-a=['1','2','3','4']
-st.selectbox('test', a)
+
 
 with col2:
 
