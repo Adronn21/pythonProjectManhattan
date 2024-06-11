f5efc4fe2c4a80de72584c4c2809eebde992c116
Adronn_ <adilbaygabulov@gmail.com>
2024-06-11 16:51:51 +0500

    test2

--- pages/Satelite_demo.py
+++ pages/Satelite_demo.py
@@ -32,8 +32,7 @@ astana_geometry = ee.Geometry.Point(71.4306, 51.1694)
 
 Map.centerObject(astana_geometry, zoom=12)
 
-with row1_col1:
-    map_state = Map.to_streamlit(height=600)
+
 
 with row1_col2:
     sat = st.selectbox("Select a satelite", list(sat_names.keys()))
@@ -44,7 +43,11 @@ with row1_col2:
 if st.button('Add layer:'):
 
     Map.addLayer(getSatelite(sat, selected_year, astana_geometry), {'bands': sat_names[sat][1], 'min': 0, 'max': 3000}, sat + str(selected_year))
-
+    with row1_col1:
+        map_state = Map.to_streamlit(height=600)
+else:
+    with row1_col1:
+        map_state = Map.to_streamlit(height=600)
 
 
 
