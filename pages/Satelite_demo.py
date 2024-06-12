70621fc8ad9f10951ed3a11d568d23eb98c9575f
Adronn_ <adilbaygabulov@gmail.com>
2024-06-12 18:37:24 +0500

    test2

--- pages/Satelite_demo.py
+++ pages/Satelite_demo.py
@@ -57,7 +57,8 @@ def get_filtered_images(satellite, year, region):
     filtered_images = collection.filterBounds(region) \
         .filterDate(f'{year}-01-01', f'{year}-12-31')
     if satellite == 'Sentinel-2':
-        return filtered_images.filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
+        return filtered_images.filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20)) \
+                            .filter(ee.Filter.lt('SNOW_ICE_PERCENTAGE', 20))
     else:
         return filtered_images.map(lambda image: mask_clouds(image, satellite))
 
