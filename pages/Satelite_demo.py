0731b29cf8a1e3ba1bb0015899ce02a4bc9a79f7
Adronn_ <adilbaygabulov@gmail.com>
2024-06-13 16:57:26 +0500

    test2

--- pages/Satelite_demo.py
+++ pages/Satelite_demo.py
@@ -88,7 +88,7 @@ Map.centerObject(region, zoom=12)
 
 
 with row1_col2:
-    brightness = st.text_input("Set brightness")
+    brightness = st.text_input("Set brightness")*1000
     sat = st.selectbox("Select a satelite", list(datasets.keys()))
     years = list(range(datasets[sat]['year_range'][0], datasets[sat]['year_range'][1]))
     selected_year = st.selectbox("Select a year", years)
