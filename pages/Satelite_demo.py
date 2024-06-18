d0604329b851baae9b9cb25d0ce6397549629b20
Adronn_ <adilbaygabulov@gmail.com>
2024-06-19 04:27:57 +0500

    test2

--- pages/Satelite_demo.py
+++ pages/Satelite_demo.py
@@ -156,8 +156,8 @@ def main():
                 # Create the plot
                 fig, ax = plt.subplots()
                 gdf.plot(ax=ax)
-                plt.xticks(rotation=90, fontsize=12)
-                plt.yticks(fontsize=12)
+                plt.xticks(rotation=90, fontsize=5)
+                plt.yticks(fontsize=5)
 
                 # Save the plot to a BytesIO object
                 buf = BytesIO()
