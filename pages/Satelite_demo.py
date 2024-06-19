11db877aeac2b964eee9f6fdd535c740bb7f29ec
Adronn_ <adilbaygabulov@gmail.com>
2024-06-19 06:03:11 +0500

    test2

--- pages/Satelite_demo.py
+++ pages/Satelite_demo.py
@@ -158,7 +158,7 @@ def main():
     if long !=0 and lat !=0:
         coords = ee.Geometry.Point([long, lat])
 
-    st.sidebar.markdown('''---OR---''')
+    st.sidebar.markdown("""---""")
 
     # Upload a zipped shapefile
     uploaded_shp_file = st.sidebar.file_uploader("Upload a Zipped Shapefile", type=["zip"])
