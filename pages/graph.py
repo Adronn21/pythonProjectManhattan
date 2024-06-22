4c30d21f7e3d68aabd4fc359f08b2ad79f6f208d
Adronn_ <adilbaygabulov@gmail.com>
2024-06-22 21:35:48 +0500

    test2

--- pages/graph.py
+++ pages/graph.py
@@ -8,8 +8,7 @@ import zipfile
 import tempfile
 import os
 from app import Navbar
-from app import datasets
-from app import indexes
+
 from app import mask_clouds
 from app import get_filtered_images
 from app import add_rgb_layer_to_map
@@ -19,6 +18,57 @@ def setup():
     st.set_page_config(layout="wide", page_title="Graph", page_icon='📈')
     st.header("📈Graph")
     return "Initialization done."
+# Datasets
+datasets = {
+    'Sentinel-2': {
+        'collection': 'COPERNICUS/S2_SR_HARMONIZED',
+        'bands': ['B4', 'B3', 'B2', 'B8', 'B5'],  # bands: 0-Red, 1-Blue, 2-Green, 3-NIR, 4-Red Edge(or red)
+        'year_range': [2019, 2023]
+    },
+    'Landsat-5': {
+        'collection': 'LANDSAT/LT05/C02/T1_L2',
+        'cloud_mask_band': 'QA_PIXEL',
+        'cloud_mask_value': 1 << 1 | 1 << 3 | 1 << 4,
+        'bands': ['SR_B3', 'SR_B2', 'SR_B1', 'SR_B4', "SR_B3"],
+        # bands: 0-Red, 1-Blue, 2-Green, 3-NIR, 4-Red Edge(or red)
+        'year_range': [1985, 2011]
+    },
+    'Landsat-7': {
+        'collection': 'LANDSAT/LE07/C02/T1_L2',
+        'cloud_mask_band': 'QA_PIXEL',
+        'cloud_mask_value': 1 << 1 | 1 << 3 | 1 << 4 | 1 << 5,
+        'bands': ['SR_B3', 'SR_B2', 'SR_B1', 'SR_B4', "SR_B3"],  # bands: 0-Red, 1-Blue, 2-Green, 3-NIR, 4-Red Edge(or red)
+        'year_range': [2000, 2023]
+    },
+    'Landsat-8': {
+        'collection': 'LANDSAT/LC08/C02/T1_L2',
+        'cloud_mask_band': 'QA_PIXEL',
+        'cloud_mask_value': 1 << 1 | 1 << 2 | 1 << 3 | 1 << 4 | 1 << 5,
+        'bands': ['SR_B4', 'SR_B3', 'SR_B2', "SR_B5", "SR_B4"],  # bands: 0-Red, 1-Blue, 2-Green, 3-NIR, 4-Red Edge(or red)
+        'year_range': [2014, 2023]
+    },
+    'MODIS': {
+        'collection': 'MODIS/006/MOD09GA',
+        'cloud_mask_band': 'state_1km',
+        'cloud_mask_value': 1 << 10 | 1 << 11,
+        'bands': ['sur_refl_b01', 'sur_refl_b04', 'sur_refl_b03', 'sur_refl_b02', 'sur_refl_b01'],  # 0-Red, 1-Blue, 2-Green, 3-NIR, 4-Red Edge(or red)
+        'year_range': [2001, 2022]
+    }
+}
+
+# Indexes
+indexes = {
+    "NDVI": "(NIR - RED) / (NIR + RED)",
+    "EVI": "2.5 * ((NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1))",
+    "SAVI": "((NIR - RED) / (NIR + RED + L)) * (1 + L)",
+    "NDWI": "(GREEN - NIR) / (GREEN + NIR)",
+    "GNDVI": "(NIR - GREEN) / (NIR + GREEN)",
+    "NDRE": "(NIR - RED_EDGE) / (NIR + RED_EDGE)",
+    "MSAVI2": "(2 * NIR + 1 - sqrt(pow((2 * NIR + 1), 2) - 8 * (NIR - RED)) ) / 2",
+    "ARVI": "(NIR - (2 * RED - BLUE)) / (NIR + (2 * RED - BLUE))",
+    "PRI": "(RED - BLUE) / (RED + BLUE)",
+    "WBI": "NIR / GREEN"
+}
 
 def main():
     # Execute setup function
