00870507dc1172c374c74c1cf66882f2444c27ae
Adronn_ <adilbaygabulov@gmail.com>
2024-06-23 20:02:39 +0500

    test2

--- pages/about.py
+++ pages/about.py
@@ -15,8 +15,8 @@ def main():
     st.markdown("<h1 style='text-align: center'>Authors:</h1>",
                         unsafe_allow_html=True)
     st.write("Baigabulov Adil"
-             "Bekenov Bekzhan"
-             "Ospan Akhmet")
+             "\nBekenov Bekzhan"
+             "\nOspan Akhmet")
     st.markdown("""Group 05-057-21-05""")
     st.markdown("""The Department of Computer Science""")
     st.markdown("""Kazakh Agro-Technical Research University""")
