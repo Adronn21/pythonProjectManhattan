e1f3d7246b181e6542ac57a3bd8726561a0b8c90
Adronn_ <adilbaygabulov@gmail.com>
2024-06-23 20:00:19 +0500

    test2

--- pages/about.py
+++ pages/about.py
@@ -14,10 +14,9 @@ def main():
 
     st.markdown("<h1 style='text-align: center'>Authors:</h1>",
                         unsafe_allow_html=True)
-    st.markdown("""Baigabulov Adil
-                   Bekenov Bekzhan""")
-    st.markdown("""Bekenov Bekzhan""")
-    st.markdown("""Ospan Akhmet""")
+    st.write("Baigabulov Adil"
+             "Bekenov Bekzhan"
+             "Ospan Akhmet")
     st.markdown("""Group 05-057-21-05""")
     st.markdown("""The Department of Computer Science""")
     st.markdown("""Kazakh Agro-Technical Research University""")
