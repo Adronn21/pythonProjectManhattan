c2064215279b4e88a92c269ddcac1c916764f3d2
Adronn_ <adilbaygabulov@gmail.com>
2024-06-23 19:44:56 +0500

    test2

--- pages/about.py
+++ pages/about.py
@@ -3,8 +3,8 @@ from app import Navbar
 
 
 def setup():
-    st.set_page_config(layout="wide", page_title="Yearly index delta Graph", page_icon='👤')
-    st.header("👤About")
+    st.set_page_config(layout="wide", page_title="Yearly index delta Graph", page_icon='📖')
+    st.header("📖About")
     return "Initialization done."
 
 def main():
