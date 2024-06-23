f5a8acab6da80731d9bb59862d93676ed12e9280
Adronn_ <adilbaygabulov@gmail.com>
2024-06-23 20:09:55 +0500

    test2

--- pages/about.py
+++ pages/about.py
@@ -12,14 +12,18 @@ def main():
     setup()
     Navbar()
 
-    st.header("Authors")
+    st.header("Authors:",
+                        unsafe_allow_html=True)
     st.write("Baigabulov Adil")
     st.write("Bekenov Bekzhan")
     st.write("Ospanov Akhmet")
+    st.markdown("<h1 style='text-align: center'>Group 05-057-21-05 \
+                <br>The Department of Computer Science \
+                <br>Kazakh Agro-Technical Research University\
+                <br>Astana</h1>",
+                unsafe_allow_html=True)
     st.markdown("""Group 05-057-21-05""")
-    st.markdown("""The Departme
-    nt of Computer Sc
-    ience""")
+    st.markdown("""The Department of Computer Science""")
     st.markdown("""Kazakh Agro-Technical Research University""")
     st.markdown("""Astana""")
 if __name__ == "__main__":
