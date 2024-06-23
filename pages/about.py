89f3deb570b42de30a266659a973c41e0b705bb8
Adronn_ <adilbaygabulov@gmail.com>
2024-06-23 19:56:05 +0500

    test2

--- pages/about.py
+++ pages/about.py
@@ -11,5 +11,14 @@ def main():
     # Execute setup function
     setup()
     Navbar()
+
+    st.markdown("""<H1>Authors:</H1>""")
+    st.markdown("""Baigabulov Adil""")
+    st.markdown("""Bekenov Bekzhan""")
+    st.markdown("""Ospan Akhmet""")
+    st.markdown("""Group 05-057-21-05""")
+    st.markdown("""The Department of Computer Science""")
+    st.markdown("""Kazakh Agro-Technical Research University""")
+    st.markdown("""Astana""")
 if __name__ == "__main__":
     main()
